93093ccec8ab2980c3a81e6bc34de7830ba8f11c
Christian Vilen <c.vilen@outlook.com>
2023-03-17 09:20:10 +0200

    pass data from backend to frontend

--- src/main/kotlin/fi/oph/ludos/LudosApplication.kt
+++ src/main/kotlin/fi/oph/ludos/LudosApplication.kt
@@ -2,10 +2,19 @@ package fi.oph.ludos
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.*
 
 @SpringBootApplication
 class LudosApplication
 
 fun main(args: Array<String>) {
     runApplication<LudosApplication>(*args)
+}
+
+@RestController
+@CrossOrigin(origins = ["http://localhost:8000"])
+@RequestMapping("api")
+class TaskController() {
+    @GetMapping("/")
+    fun index() = "{\"message\":\"hello ludos\"}"
 }
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,20 @@
 import './App.scss'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await fetch('http://localhost:8080/api/', { method: 'GET' })
+      const json = await response.json()
+      setData(json.message)
+    })()
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hello ludos</h1>
+      <h1>{data}</h1>
     </div>
   )
 }
