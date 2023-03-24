3703c496a91b3ca5614cc9808e7e47adde2721b2
Christian Vilen <c.vilen@outlook.com>
2023-03-24 14:37:13 +0200

    feat: add color variables

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,18 +2,7 @@
 const tailwindConfig = {
   content: ['./src/**/*.{ts,tsx}'], theme: {
     colors: {
-      gray: {
-        900: '#202225',
-        800: '#2f3136',
-        700: '#36393f',
-        600: '#4f545c',
-        400: '#d4d7dc',
-        300: '#e3e5e8',
-        200: '#ebedef',
-        100: '#f2f3f5'
-      }, omaVari: {
-        100: '#FD5532'
-      }
+      green: { primary: '#3A7A10', light: '#5BCA13' }, gray: { primary: '#000000', light: '#4B4B4B' }
     }, extend: {}
   }, plugins: []
 }
