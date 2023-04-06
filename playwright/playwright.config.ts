e2b7ceea6ce972bfa9d3ca057948cf9306af8fe1
Christian Vilen <c.vilen@outlook.com>
2023-04-06 17:02:40 +0300

    Switch port where tests are ran and abort on failure

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.WEB_URL || 'http://localhost:8000',
+    baseURL: 'http://localhost:8000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
