36c130b1a420dd885816129e2e47663cc6dbb21a
Christian Vilen <c.vilen@outlook.com>
2023-04-20 09:48:52 +0300

    Footer functionalities

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,8 +1,11 @@
-import { FC } from 'react'
 import { TFunction } from 'i18next'
 import logo from 'web/assets/oph_fin_vaaka.png'
+import { feedbackKey } from './routes/routes'
+import { ExternalLink } from './ExternalLink'
+import { InternalLink } from './InternalLink'
+import { TIETOSUOJA_SELOSTE_URL } from '../constants'
 
-export const Footer: FC<{ t: TFunction }> = ({ t }) => (
+export const Footer = ({ t }: { t: TFunction }) => (
   <div className="mt-5 border-t-2 border-gray-separator pb-4 pt-3">
     <div className="flex flex-wrap md:px-10">
       <div className="flex w-full flex-col md:w-3/12">
@@ -11,14 +14,11 @@ export const Footer: FC<{ t: TFunction }> = ({ t }) => (
         </div>
       </div>
       <span className="hidden md:flex md:w-2/12 md:flex-col" />
-      <div className="mt-3 flex w-1/3 flex-col text-center md:w-2/12 md:text-left">
-        <p className="text-green-primary">{t('footer.palaute')}</p>
+      <div className="mt-3 flex w-1/2 flex-col text-center md:w-2/12 md:text-left">
+        <InternalLink to={feedbackKey}>{t('footer.palaute')}</InternalLink>
       </div>
-      <div className="mt-3 flex w-1/3 flex-col text-center md:w-2/12 md:text-left">
-        <p className="text-green-primary">{t('footer.kayttoehdot')}</p>
-      </div>
-      <div className="mt-3 flex w-1/3 flex-col text-center md:w-3/12 md:text-left">
-        <p className="text-green-primary">{t('footer.tietosuoja')}</p>
+      <div className="mt-3 flex w-1/2 flex-col text-center md:w-3/12 md:text-left">
+        <ExternalLink url={TIETOSUOJA_SELOSTE_URL}>{t('footer.tietosuoja')}</ExternalLink>
       </div>
     </div>
   </div>
