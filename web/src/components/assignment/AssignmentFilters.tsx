38ffb6c0f42ede863621ec139146f2326a38784b
Christian Vilen <c.vilen@outlook.com>
2023-04-27 10:50:57 +0300

    Close dropdowns on click to parent element

--- web/src/components/assignment/AssignmentFilters.tsx
+++ web/src/components/assignment/AssignmentFilters.tsx
@@ -2,6 +2,8 @@ import { Dispatch, SetStateAction, useState } from 'react'
 import { FiltersType } from './useFilters'
 import { Dropdown } from '../Dropdown'
 import { SUKO_ASSIGNMENT_OPTIONS, SUKO_ASSIGNMENT_ORDER_OPTIONS } from '../../koodisto'
+import { useTranslation } from 'react-i18next'
+import { useDropdownCloseOnBlur } from '../../hooks/useDropdownCloseOnBlur'
 
 const defaultExpandedState = {
   course: false,
@@ -19,7 +21,9 @@ type AssignmentFiltersProps = {
 }
 
 export const AssignmentFilters = ({ filters, setFilters }: AssignmentFiltersProps) => {
+  const { t } = useTranslation()
   const [isExpanded, setExpansion] = useState(defaultExpandedState)
+  const dropdownRef = useDropdownCloseOnBlur<typeof defaultExpandedState>(defaultExpandedState, setExpansion)
 
   const handleDropdownClick = (dropdownName: keyof typeof defaultExpandedState, bool: boolean) => {
     setExpansion(defaultExpandedState)
@@ -30,11 +34,11 @@ export const AssignmentFilters = ({ filters, setFilters }: AssignmentFiltersProp
     setFilters((curr) => ({ ...curr, [key]: value }))
 
   return (
-    <div className="border border-gray-light bg-gray-bg">
-      <p className="px-2 py-1">Suodata tehtäviä</p>
+    <div className="border border-gray-light bg-gray-bg" ref={dropdownRef}>
+      <p className="px-2 py-1">{t('filter.title')}</p>
       <div className="row w-full flex-wrap justify-start">
         <div className="w-full md:w-56">
-          <p className="pl-2">Oppimäärä</p>
+          <p className="pl-2">{t('filter.oppimaara')}</p>
           <Dropdown
             currentOption={filters.course}
             isExpanded={isExpanded.course}
@@ -44,7 +48,7 @@ export const AssignmentFilters = ({ filters, setFilters }: AssignmentFiltersProp
           />
         </div>
         <div className="w-full md:w-56">
-          <p className="pl-2">Tehtävätyyppi</p>
+          <p className="pl-2">{t('filter.tyyppi')}</p>
           <Dropdown
             currentOption={SUKO_ASSIGNMENT_OPTIONS.find((opt) => opt.key === filters.assignmentType)?.value || null}
             isExpanded={isExpanded.assignmentType}
@@ -54,7 +58,7 @@ export const AssignmentFilters = ({ filters, setFilters }: AssignmentFiltersProp
           />
         </div>
         <div className="w-full md:w-56">
-          <p className="pl-2">Aihe</p>
+          <p className="pl-2">{t('filter.aihe')}</p>
           <Dropdown
             currentOption={null}
             isExpanded={isExpanded.topic}
@@ -64,17 +68,18 @@ export const AssignmentFilters = ({ filters, setFilters }: AssignmentFiltersProp
           />
         </div>
         <div className="w-full md:w-40">
-          <p className="pl-2">Tehtävän kieli</p>
+          <p className="pl-2">{t('filter.kieli')}</p>
           <Dropdown
             currentOption={null}
             isExpanded={isExpanded.language}
             setExpansion={(bool) => handleDropdownClick('language', bool)}
             onOptionClick={(opt) => handleFilterChange('language', opt)}
             options={[]}
+            canReset={false}
           />
         </div>
         <div className="w-full md:w-40">
-          <p className="pl-2">Järjestä</p>
+          <p className="pl-2">{t('filter.jarjesta')}</p>
           <Dropdown
             currentOption={
               SUKO_ASSIGNMENT_ORDER_OPTIONS.find((opt) => opt.key === filters.orderDirection)?.value || null
@@ -83,6 +88,7 @@ export const AssignmentFilters = ({ filters, setFilters }: AssignmentFiltersProp
             setExpansion={(bool) => handleDropdownClick('order', bool)}
             onOptionClick={(opt) => handleFilterChange('orderDirection', opt)}
             options={SUKO_ASSIGNMENT_ORDER_OPTIONS}
+            canReset={false}
           />
         </div>
       </div>
