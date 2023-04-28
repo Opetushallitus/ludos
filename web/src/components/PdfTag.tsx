1750bba82d41eede6e67a995e69b87845bcee021
Christian Vilen <c.vilen@outlook.com>
2023-04-28 15:06:49 +0300

    Create spinner and pdf tag components

---
+++ web/src/components/PdfTag.tsx
@@ -0,0 +1,8 @@
+import { Icon } from './Icon'
+
+export const PdfTag = () => (
+  <span className="row my-auto gap-2 rounded bg-gray-bg px-2">
+    <Icon name="pdf" color="text-black" />
+    <p className="text-xss">Pdf</p>
+  </span>
+)
