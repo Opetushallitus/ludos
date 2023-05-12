fc5136a3229bf4619fcbc9b3fc517b5931e8333d
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2023-05-12 18:07:06 +0300

    Revert "Show a proper error message for un/misconfigured profile"
    This does not work with gradle

--- server/src/main/kotlin/fi/oph/ludos/LudosApplication.kt
+++ server/src/main/kotlin/fi/oph/ludos/LudosApplication.kt
@@ -1,6 +1,5 @@
 package fi.oph.ludos
 
-import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.FilterRegistrationBean
@@ -8,38 +7,17 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.web.filter.ForwardedHeaderFilter
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.resource.PathResourceResolver
-import kotlin.system.exitProcess
 
 
 @SpringBootApplication
 class LudosApplication
 
 fun main(args: Array<String>) {
-    validateProfile()
     runApplication<LudosApplication>(*args)
-}
-
-fun validateProfile() {
-    if (System.getProperty("ludos.profileValidated") == "true") {
-        return
-    }
-    val logger = LoggerFactory.getLogger(LudosApplication::class.java)
-    val supportedProfiles = PathMatchingResourcePatternResolver().getResources("classpath*:application-*.yml")
-        .map { it.filename?.replace("application-", "")?.replace(".yml", "") }
-        .sortedBy { it }
-    val activeProfiles = System.getProperty("spring.profiles.active")
-    logger.info("Supported profiles: $supportedProfiles")
-    logger.info("spring.profiles.active: '$activeProfiles'")
-    if (supportedProfiles.find { it == activeProfiles } == null) {
-        logger.error("FATAL ERROR: Spring profile not set or is not supported")
-        exitProcess(1)
-    }
-    System.setProperty("ludos.profileValidated", "true")
 }
 
 @Configuration
