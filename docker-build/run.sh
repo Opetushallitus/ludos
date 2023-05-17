29e887a091a1826f6523eea593f0aab92da18a56
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2023-05-17 14:35:25 +0300

    Docker: run ludos with java instead of gradle
    So no gradle dependency needed in docker image, and running through
    gradle is probably a bad idea in production anyway.

--- docker-build/run.sh
+++ docker-build/run.sh
@@ -2,5 +2,10 @@
 
 set -euo pipefail
 
-cd server
-./gradlew bootRun --args="--spring.profiles.active=$ENV_NAME"
+if [ -r "ludos.jar" ]; then
+    JAR_PATH=ludos.jar
+else
+    JAR_PATH="$(dirname "$0")/../server/build/libs/ludos-0.0.1-SNAPSHOT.jar"
+fi
+
+java -jar -Dspring.profiles.active="$ENV_NAME" "$JAR_PATH"
