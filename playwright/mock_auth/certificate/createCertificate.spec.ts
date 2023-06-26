d80d31534400d293c14f5f8ab132a9b2ec22fe53
Christian Vilen <c.vilen@outlook.com>
2023-06-26 09:03:41 +0300

    Extract duplicated code and and parameterize it for different cases

--- playwright/mock_auth/certificate/createCertificate.spec.ts
+++ playwright/mock_auth/certificate/createCertificate.spec.ts
@@ -1,6 +1,13 @@
 import { Page, expect, test } from '@playwright/test'
 import path from 'path'
 
+async function beforeEach(page: Page, exam: string) {
+  await page.goto('/')
+  await page.getByTestId(`nav-link-${exam}`).click()
+  await page.getByTestId('tab-todistukset').click()
+  await page.getByTestId('create-todistus-button').click()
+}
+
 async function uploadFile(page: Page) {
   const filePath = path.resolve(__dirname, '../../fixtures/fixture.pdf')
 
@@ -17,125 +24,52 @@ async function uploadFile(page: Page) {
   await expect(page.getByTestId('fixture.pdf')).toHaveText(`fixture.pdfopen_in_new${formattedDate}`)
 }
 
-test.describe('Suko certificate form tests', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/')
-    await page.getByTestId('nav-link-suko').click()
-    await page.getByTestId('tab-todistukset').click()
-    await page.getByTestId('create-todistus-button').click()
-  })
-
-  test('can create a new Suko certification', async ({ page }) => {
-    await page.getByTestId('nameFi').click()
-    await page.getByTestId('nameFi').fill('Testi todistus')
-    await page.getByTestId('nameFi').press('Tab')
-    await page.getByTestId('contentFi').fill('Todistuksen kuvaus')
-
-    await uploadFile(page)
-
-    await page.getByTestId('form-submit').click()
-
-    const header = page.getByTestId('assignment-header')
-
-    await expect(header).toHaveText('Testi todistus')
-  })
-
-  test('can create draft certificate', async ({ page }) => {
-    const btn = page.getByTestId('form-draft')
-    await expect(btn).toHaveText('Tallenna luonnoksena')
-
-    await btn.click()
-
-    page.getByText('Luonnos', { exact: true })
-  })
-
-  test('can cancel certificate creation', async ({ page }) => {
+async function createCertificate(page: Page, certificateType: string) {
+  if (certificateType === 'cancel') {
     const btn = page.getByTestId('form-cancel')
     await expect(btn).toHaveText('Peruuta')
-
     await btn.click()
-    page.getByTestId('create-todistus-button')
-  })
+    await page.getByTestId('create-todistus-button')
+  }
+
+  await page.getByTestId('nameFi').click()
+  await page.getByTestId('nameFi').fill('Testi todistus')
+  await page.getByTestId('contentFi').fill('Todistuksen kuvaus')
+
+  await uploadFile(page)
+
+  if ('submit') {
+    await page.getByTestId('form-submit').click()
+    const header = page.getByTestId('assignment-header')
+    await expect(header).toHaveText('Testi todistus')
+  } else if (certificateType === 'draft') {
+    const btn = page.getByTestId('form-draft')
+    await expect(btn).toHaveText('Tallenna luonnoksena')
+    await btn.click()
+    await page.getByText('Luonnos', { exact: true })
+  }
+}
+
+test.describe('Suko certificate form tests', () => {
+  test.beforeEach(async ({ page }) => await beforeEach(page, 'suko'))
+
+  test('can create a new Suko certification', async ({ page }) => await createCertificate(page, 'submit'))
+  test('can create draft certificate', async ({ page }) => await createCertificate(page, 'draft'))
+  test('can cancel certificate creation', async ({ page }) => await createCertificate(page, 'cancel'))
 })
 
 test.describe('Puhvi certificate form tests', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/')
-    await page.getByTestId('nav-link-puhvi').click()
-    await page.getByTestId('tab-todistukset').click()
-    await page.getByTestId('create-todistus-button').click()
-  })
+  test.beforeEach(async ({ page }) => await beforeEach(page, 'puhvi'))
 
-  test('can create a new Puhvi certification', async ({ page }) => {
-    await page.getByTestId('nameFi').click()
-    await page.getByTestId('nameFi').fill('Testi todistus')
-    await page.getByTestId('nameFi').press('Tab')
-    await page.getByTestId('contentFi').fill('Todistuksen kuvaus')
-
-    await uploadFile(page)
-
-    await page.getByTestId('form-submit').click()
-
-    const header = page.getByTestId('assignment-header')
-
-    await expect(header).toHaveText('Testi todistus')
-  })
-
-  test('can create draft certificate', async ({ page }) => {
-    const btn = page.getByTestId('form-draft')
-    await expect(btn).toHaveText('Tallenna luonnoksena')
-
-    await btn.click()
-
-    page.getByText('Luonnos', { exact: true })
-  })
-
-  test('can cancel certificate creation', async ({ page }) => {
-    const btn = page.getByTestId('form-cancel')
-    await expect(btn).toHaveText('Peruuta')
-
-    await btn.click()
-    page.getByTestId('create-todistus-button')
-  })
+  test('can create a new Puhvi certification', async ({ page }) => await createCertificate(page, 'submit'))
+  test('can create draft certificate', async ({ page }) => await createCertificate(page, 'draft'))
+  test('can cancel certificate creation', async ({ page }) => await createCertificate(page, 'cancel'))
 })
 
 test.describe('Ld certificate form tests', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/')
-    await page.getByTestId('nav-link-ld').click()
-    await page.getByTestId('tab-todistukset').click()
-    await page.getByTestId('create-todistus-button').click()
-  })
+  test.beforeEach(async ({ page }) => await beforeEach(page, 'ld'))
 
-  test('can create a new Ld certification', async ({ page }) => {
-    await page.getByTestId('nameFi').click()
-    await page.getByTestId('nameFi').fill('Testi todistus')
-    await page.getByTestId('nameFi').press('Tab')
-    await page.getByTestId('contentFi').fill('Todistuksen kuvaus')
-
-    await uploadFile(page)
-
-    await page.getByTestId('form-submit').click()
-
-    const header = page.getByTestId('assignment-header')
-
-    await expect(header).toHaveText('Testi todistus')
-  })
-
-  test('can create draft certificate', async ({ page }) => {
-    const btn = page.getByTestId('form-draft')
-    await expect(btn).toHaveText('Tallenna luonnoksena')
-
-    await btn.click()
-
-    page.getByText('Luonnos', { exact: true })
-  })
-
-  test('can cancel certificate creation', async ({ page }) => {
-    const btn = page.getByTestId('form-cancel')
-    await expect(btn).toHaveText('Peruuta')
-
-    await btn.click()
-    page.getByTestId('create-todistus-button')
-  })
+  test('can create a new Ld certification', async ({ page }) => await createCertificate(page, 'submit'))
+  test('can create draft certificate', async ({ page }) => await createCertificate(page, 'draft'))
+  test('can cancel certificate creation', async ({ page }) => await createCertificate(page, 'cancel'))
 })
