547d86ebc3dcd86f46ad28d804d0aa57cb40d4f9
Christian Vilen <c.vilen@outlook.com>
2023-07-31 16:36:07 +0300

    Adjust e2e test for new text

--- playwright/all_tests/cas_auth/login.spec.ts
+++ playwright/all_tests/cas_auth/login.spec.ts
@@ -10,9 +10,7 @@ test('yllapitaja can login and logout', async ({ page }) => {
   await page.locator('#username').fill(process.env.TESTIKAYTTAJA_YLLAPITAJA_USERNAME!)
   await page.locator('#password').fill(process.env.TESTIKAYTTAJA_YLLAPITAJA_PASSWORD!)
   await page.locator('input[type=submit]').click()
-  await expect(page.getByTestId('page-heading-etusivu')).toContainText(
-    'Hei Ludos Ylläpitäjä, tervetuloa Koepankin ylläpitoon!'
-  )
+  await expect(page.getByTestId('page-heading-etusivu')).toContainText('Hei Ludos, tervetuloa LUDOS -palveluun!')
 
   await page.getByTestId('header-user-dropdown-expand').click()
   await page.getByTestId('logout-button').click()
