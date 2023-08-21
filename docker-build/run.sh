2214bac67db469f6465cf3e9b110973aee2d1b48
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2023-08-21 15:30:07 +0300

    One less layer in docker image

--- docker-build/run.sh
+++ docker-build/run.sh
@@ -8,7 +8,7 @@ fi
 if [ -r "ludos.jar" ]; then
     JAR_PATH=ludos.jar
 else
-    JAR_PATH="$(dirname "$0")/../server/build/libs/ludos-0.0.1-SNAPSHOT.jar"
+    JAR_PATH="$(dirname "$0")/../server/build/libs/ludos.jar"
 fi
 
 java -jar -Dspring.profiles.active="$LUDOS_PROFILES" "$JAR_PATH"
