2ea516b0caf777d3cdaf616819c38583a2e83e4a
Christian Vilen <c.vilen@outlook.com>
2023-08-24 14:13:08 +0300

    adjust cols and add word breaks

--- web/src/components/contentpage/CertificateContent.tsx
+++ web/src/components/contentpage/CertificateContent.tsx
@@ -1,11 +1,11 @@
-import { CertificateIn } from '../../types'
+import { CertificateIn, ContentTypeEng } from '../../types'
 import { useTranslation } from 'react-i18next'
-import { AttachmentDetails } from '../exam/formCommon/attachment/AttachmentDetails'
-import { DOWNLOAD_CERTIFICATE_ATTACHMENT_URL, DOWNLOAD_INSTRUCTION_ATTACHMENT_URL } from '../../constants'
+import { AttachmentFileDetailView } from '../exam/formCommon/attachment/AttachmentFileDetailView'
 
 type CertificateContentProps = {
   certificate: CertificateIn
 }
+
 export const CertificateContent = ({ certificate }: CertificateContentProps) => {
   const { t } = useTranslation()
 
@@ -16,23 +16,20 @@ export const CertificateContent = ({ certificate }: CertificateContentProps) => 
       <h3 className="mt-6 font-semibold">{t('certificate.kuvaus')}</h3>
       <p>{certificate.description}</p>
       <h3 className="mb-3 mt-8 font-semibold">{t('certificate.todistus')}</h3>
-      <div className="w-full md:w-3/4">
-        <div className="grid grid-cols-11 gap-2">
-          <p className="col-span-3 md:col-span-4">{t('file.nimi')}</p>
-          <p className="col-span-2 md:col-span-2 md:block">{t('file.lisatty')}</p>
-        </div>
-
-        <div className="border-y border-gray-light" />
-        <AttachmentDetails
-          attachmentDownloadUrlPrefix={DOWNLOAD_CERTIFICATE_ATTACHMENT_URL}
-          fileDetails={{
-            fileUploadDate: certificate.attachment.fileUploadDate,
+      <AttachmentFileDetailView
+        contentType={ContentTypeEng.TODISTUKSET}
+        attachments={{
+          name: certificate.attachment.name,
+          attachment: {
             name: certificate.attachment.name,
             fileKey: certificate.attachment.fileKey,
-            fileName: certificate.attachment.fileName
-          }}
-        />
-      </div>
+            fileName: certificate.attachment.fileName,
+            language: certificate.attachment.language,
+            fileUploadDate: certificate.attachment.fileUploadDate
+          }
+        }}
+        handleAttachmentNameChange={() => {}}
+      />
     </div>
   )
 }
--- web/src/components/exam/formCommon/attachment/AttachmentFileDetailView.tsx
+++ web/src/components/exam/formCommon/attachment/AttachmentFileDetailView.tsx
@@ -54,9 +54,9 @@ export const AttachmentFileDetailView = ({
   return (
     <div className="w-full md:w-3/4">
       <div className="grid grid-cols-11 gap-2">
-        <p className="col-span-3 md:col-span-4">{t('file.nimi')}</p>
+        <p className="col-span-5 break-all md:col-span-5">{t('file.nimi')}</p>
         {fileDetails && Array.isArray(fileDetails) && (
-          <p className="col-span-4 md:col-span-4">{t('file.liitteen-nimi')}</p>
+          <p className="col-span-4 break-all md:col-span-4">{t('file.liitteen-nimi')}</p>
         )}
         <p className="col-span-2 md:col-span-2 md:block">{t('file.lisatty')}</p>
       </div>
