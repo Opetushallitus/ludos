209ed6ce6bb72742e4a53c82b1e5f1cbacdaf3f8
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2023-08-31 14:58:08 +0300

    Fix scroll bar even on short pages

--- web/src/components/layout/Layout.tsx
+++ web/src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ interface TLayout {
 }
 
 export const Layout = ({ header, footer, children }: TLayout) => (
-  <main className="grid min-h-screen max-w-full grid-rows-[6rem,1fr,7rem] gap-0">
+  <main className="grid min-h-[98vh] max-w-full grid-rows-[6rem,1fr,7rem] gap-0">
     <header className="border-t-5 border-green-primary">{header}</header>
     <div className="flex justify-center">
       <div className="w-[80vw]">
