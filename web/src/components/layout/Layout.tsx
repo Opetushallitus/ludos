f490df5ba03e82df6f2f7e8ae9bbcd0b7efc6502
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2023-08-31 14:44:28 +0300

    Add green line to top of all pages
    As per Arto's proto

--- web/src/components/layout/Layout.tsx
+++ web/src/components/layout/Layout.tsx
@@ -8,7 +8,7 @@ interface TLayout {
 
 export const Layout = ({ header, footer, children }: TLayout) => (
   <main className="grid min-h-screen max-w-full grid-rows-[6rem,1fr,7rem] gap-0">
-    <header>{header}</header>
+    <header className="border-t-5 border-green-primary">{header}</header>
     <div className="flex justify-center">
       <div className="w-[80vw]">
         <section>{children}</section>
