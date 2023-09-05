b0fc42ccbd3e453b8640d66746bb2f711fb23b73
Christian Vilen <c.vilen@outlook.com>
2023-09-05 13:43:27 +0300

    Fix index.html title koepankki -> ludos

--- web/src/hooks/useDocumentLanguage.ts
+++ web/src/hooks/useDocumentLanguage.ts
@@ -2,9 +2,10 @@ import { useTranslation } from 'react-i18next'
 import { useEffect } from 'react'
 
 export function useDocumentLanguage() {
-  const { i18n } = useTranslation()
+  const { i18n, t } = useTranslation()
 
   useEffect(() => {
     document.documentElement.lang = i18n.language
-  }, [i18n.language])
+    document.title = t('title.ludos')
+  }, [i18n.language, t])
 }
