ddfafdf35b9aff85cd5e6e5c9129cf2f325d1e43
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2023-09-06 12:34:42 +0300

    Properly close resources in Koodisto and Lozalization repositories

--- server/src/main/kotlin/fi/oph/ludos/koodisto/KoodistoRepository.kt
+++ server/src/main/kotlin/fi/oph/ludos/koodisto/KoodistoRepository.kt
@@ -1,10 +1,10 @@
 package fi.oph.ludos.koodisto
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import fi.oph.ludos.ludosHttpClientBuilder
 import org.apache.http.HttpStatus
-import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.impl.client.CloseableHttpClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Repository
 
@@ -13,25 +13,24 @@ class KoodistoRepository(
     private val objectMapper: ObjectMapper,
     @Value("\${ludos.opintopolkuHostname}") private val opintopolkuHostname: String
 ) {
-    private val httpClient: HttpClient = HttpClientBuilder.create().build()
+    private val httpClient: CloseableHttpClient = ludosHttpClientBuilder().build()
 
     private val baseUrl = "https://${opintopolkuHostname}/koodisto-service/rest/json"
 
     fun getKoodistoFromKoodistopalvelu(koodisto: KoodistoName): Array<Koodi> {
         val url = "$baseUrl/${koodisto.koodistoUri}/koodi?onlyValidKoodis=true"
         val httpGet = HttpGet(url)
-        val response = httpClient.execute(httpGet)
+        return httpClient.execute(httpGet).use { response ->
+            if (response.statusLine.statusCode != HttpStatus.SC_OK) {
+                throw RuntimeException("Unexpected status code ${response.statusLine.statusCode} when fetcing koodisto ${koodisto}")
+            }
 
-        if (response.statusLine.statusCode != HttpStatus.SC_OK) {
-            throw RuntimeException("Unexpected status code ${response.statusLine.statusCode} when fetcing koodisto ${koodisto}")
+            objectMapper.readValue(response.entity.content.bufferedReader(), Array<Koodi>::class.java)
         }
-
-        return objectMapper.readValue(response.entity.content.bufferedReader(), Array<Koodi>::class.java)
     }
 
     fun getKoodistoFromResource(koodisto: KoodistoName): Array<Koodi> {
         val resourceStream = Thread.currentThread().contextClassLoader.getResourceAsStream("backup_data/koodisto_${koodisto.koodistoUri}.json")
         return objectMapper.readValue(resourceStream, Array<Koodi>::class.java)
     }
-
 }
--- server/src/main/kotlin/fi/oph/ludos/localization/LocalizationRepository.kt
+++ server/src/main/kotlin/fi/oph/ludos/localization/LocalizationRepository.kt
@@ -2,11 +2,10 @@ package fi.oph.ludos.localization
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import org.apache.http.HttpResponse
+import fi.oph.ludos.ludosHttpClientBuilder
 import org.apache.http.HttpStatus
-import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.impl.client.CloseableHttpClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Repository
 
@@ -15,18 +14,18 @@ class LocalizationRepository(
     @Value("\${ludos.opintopolkuHostname}") private val opintopolkuHostname: String,
     private val objectMapper: ObjectMapper
 ) {
-    private val httpClient: HttpClient = HttpClientBuilder.create().build()
+    private val httpClient: CloseableHttpClient = ludosHttpClientBuilder().build()
     private val url = "https://${opintopolkuHostname}/lokalisointi/cxf/rest/v1/localisation?category=ludos"
 
     fun getLocalizationTextsFromLokalisointipalvelu(): Array<Localization> {
         val request = HttpGet(url)
-        val response: HttpResponse = httpClient.execute(request)
+        return httpClient.execute(request).use { response ->
+            if (response.statusLine.statusCode != HttpStatus.SC_OK) {
+                throw RuntimeException("Unexpected status code ${response.statusLine.statusCode} when fetcing localizations")
+            }
 
-        if (response.statusLine.statusCode != HttpStatus.SC_OK) {
-            throw RuntimeException("Unexpected status code ${response.statusLine.statusCode} when fetcing localizations")
+            objectMapper.readValue<Array<Localization>>(response.entity.content.bufferedReader())
         }
-
-        return objectMapper.readValue<Array<Localization>>(response.entity.content.bufferedReader())
     }
 
     fun getLocalizationTextsFromResourceFile(): Array<Localization> {
