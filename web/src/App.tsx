e2b5e4852dcf7d94453257bce9a922e42403a511
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2023-09-18 09:45:07 +0300

    Rename Spring Security continue param to j and hide it in frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,9 +2,11 @@ import { BrowserRouter } from 'react-router-dom'
 import { LudosRoutes } from './components/routes/LudosRoutes'
 import { useDocumentLanguage } from './hooks/useDocumentLanguage'
 import { LudosContextProvider } from './components/LudosContextProvider'
+import { useHideSpringSecurityMatchingRequestParameter } from './hooks/useHideSpringSecurityMatchingRequestParameter'
 
 const App = () => {
   useDocumentLanguage()
+  useHideSpringSecurityMatchingRequestParameter()
 
   return (
     <BrowserRouter>
