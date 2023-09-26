1e32fe30f11256ae496ba958295c54942bc35a9d
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2023-09-26 13:08:14 +0300

    Remove unused assignment_content_content_id column

--- server/src/main/resources/db/migration/V0012__multiple_content_fields_for_ld_assignment.sql
+++ server/src/main/resources/db/migration/V0012__multiple_content_fields_for_ld_assignment.sql
@@ -1,23 +1,21 @@
 CREATE TABLE assignment_content
 (
-    assignment_content_content_id         serial PRIMARY KEY,
-    assignment_id      integer REFERENCES assignment (assignment_id),
+    assignment_id                         integer REFERENCES assignment (assignment_id),
     assignment_content_language           language,
+    assignment_content_order_index        integer NOT NULL,
     assignment_content_assignment_content text    NOT NULL,
-    assignment_content_order_index                integer NOT NULL,
-    CONSTRAINT assignment_id_content_language_unique UNIQUE (assignment_id, assignment_content_language)
+    PRIMARY KEY (assignment_id, assignment_content_language, assignment_content_order_index)
 );
 
 -- suko
 CREATE TABLE suko_assignment_content
 (
-    PRIMARY KEY (assignment_content_content_id),
+    PRIMARY KEY (assignment_id, assignment_content_language, assignment_content_order_index),
     CONSTRAINT suko_assignment_content_assignment_id_fkey FOREIGN KEY (assignment_id)
         REFERENCES suko_assignment (assignment_id)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 ) INHERITS (assignment_content);
-CREATE INDEX suko_assignment_content_content_id_idx ON suko_assignment_content (assignment_content_content_id);
 -- Migrate contentFi
 INSERT INTO suko_assignment_content (assignment_id, assignment_content_language,
                                      assignment_content_assignment_content, assignment_content_order_index)
@@ -32,13 +30,12 @@ FROM suko_assignment a;
 -- ld
 CREATE TABLE ld_assignment_content
 (
-    PRIMARY KEY (assignment_content_content_id),
+    PRIMARY KEY (assignment_id, assignment_content_language, assignment_content_order_index),
     CONSTRAINT ld_assignment_content_assignment_id_fkey FOREIGN KEY (assignment_id)
         REFERENCES ld_assignment (assignment_id)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 ) INHERITS (assignment_content);
-CREATE INDEX ld_assignment_content_content_id_idx ON ld_assignment_content (assignment_content_content_id);
 -- Migrate contentFi
 INSERT INTO ld_assignment_content (assignment_id, assignment_content_language,
                                    assignment_content_assignment_content, assignment_content_order_index)
@@ -53,13 +50,12 @@ FROM ld_assignment a;
 -- puhvi
 CREATE TABLE puhvi_assignment_content
 (
-    PRIMARY KEY (assignment_content_content_id),
+    PRIMARY KEY (assignment_id, assignment_content_language, assignment_content_order_index),
     CONSTRAINT puhvi_assignment_content_assignment_id_fkey FOREIGN KEY (assignment_id)
         REFERENCES puhvi_assignment (assignment_id)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 ) INHERITS (assignment_content);
-CREATE INDEX puhvi_assignment_content_content_id_idx ON puhvi_assignment_content (assignment_content_content_id);
 -- Migrate contentFi
 INSERT INTO puhvi_assignment_content (assignment_id, assignment_content_language,
                                       assignment_content_assignment_content, assignment_content_order_index)
@@ -73,5 +69,7 @@ FROM puhvi_assignment a;
 
 
 -- drop assignment_content from assignment
-ALTER TABLE assignment DROP COLUMN assignment_content_fi;
-ALTER TABLE assignment DROP COLUMN assignment_content_sv;
+ALTER TABLE assignment
+    DROP COLUMN assignment_content_fi;
+ALTER TABLE assignment
+    DROP COLUMN assignment_content_sv;
