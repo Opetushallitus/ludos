0ca37bb9c148e4bcd017400cac5a2234c9cb4adb
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2023-11-09 14:03:44 +0200

    Remove DataWrapper

--- web/src/components/content/Content.tsx
+++ web/src/components/content/Content.tsx
@@ -33,7 +33,7 @@ const Content = ({ exam, isPresentation }: ContentProps) => {
   const teachingLanguageOverrideIfSukoAssignment =
     exam === Exam.SUKO && contentType === ContentType.koetehtavat ? TeachingLanguage.fi : teachingLanguage
 
-  const { DataWrapper } = useFetch<BaseOut>(`${ContentTypeSingularEng[contentType!]}/${exam}/${id}`)
+  const { data, error } = useFetch<BaseOut>(`${ContentTypeSingularEng[contentType!]}/${exam}/${id}`)
 
   const handleNavigation = useCallback(() => {
     if (state?.returnLocation) {
@@ -43,66 +43,68 @@ const Content = ({ exam, isPresentation }: ContentProps) => {
     }
   }, [contentType, exam, navigate, state?.returnLocation])
 
+  if (error) {
+    return <ContentError contentType={contentType!} error={error} />
+  }
+  if (!data || !contentType) {
+    return null
+  }
+
   return (
     <div className="min-h-[80vh]">
-      <DataWrapper
-        errorEl={<>{contentType && <ContentError contentType={contentType} />}</>}
-        render={(data) => (
-          <div className="row">
-            <div className="col w-full pr-5 md:w-9/12">
-              <div className="row pb-3">
-                <div className="col min-h-[40vh] w-full">
-                  <ContentHeader
-                    teachingLanguage={teachingLanguageOverrideIfSukoAssignment}
-                    setTeachingLanguage={setTeachingLanguage}
-                    data={data}
-                    contentType={contentType!}
-                    isPresentation={isPresentation}
-                  />
-                  {!isPresentation && isYllapitaja && (
-                    <div className="row">
-                      <StateTag state={data.publishState} />
-                      <InternalLink
-                        className="row ml-3 gap-1 hover:cursor-pointer hover:underline"
-                        to={editingFormPath(exam, contentType!, data.id)}
-                        data-testid="edit-content-btn">
-                        <Icon name="muokkaa" color="text-green-primary" />
-                        <p className="text-green-primary">{t('assignment.muokkaa')}</p>
-                      </InternalLink>
-                    </div>
-                  )}
-                  {contentType && isAssignment(data, contentType) && (
-                    <AssignmentContent
-                      assignment={data}
-                      exam={exam}
-                      teachingLanguage={teachingLanguageOverrideIfSukoAssignment}
-                      isPresentation={isPresentation}
-                    />
-                  )}
-
-                  {contentType && isCertificate(data, contentType) && <CertificateContent certificate={data} />}
-
-                  {contentType && isInstruction(data, contentType) && (
-                    <InstructionContent
-                      instruction={data}
-                      teachingLanguage={teachingLanguage}
-                      isPresentation={isPresentation}
-                    />
-                  )}
-                </div>
-              </div>
-              {!isPresentation && (
-                <div className="row mb-6">
-                  <Button variant="buttonSecondary" onClick={handleNavigation} data-testid="return">
-                    {lt.returnTextByContentType[contentType!]}
-                  </Button>
+      <div className="row">
+        <div className="col w-full pr-5 md:w-9/12">
+          <div className="row pb-3">
+            <div className="col min-h-[40vh] w-full">
+              <ContentHeader
+                teachingLanguage={teachingLanguageOverrideIfSukoAssignment}
+                setTeachingLanguage={setTeachingLanguage}
+                data={data}
+                contentType={contentType}
+                isPresentation={isPresentation}
+              />
+              {!isPresentation && isYllapitaja && (
+                <div className="row">
+                  <StateTag state={data.publishState} />
+                  <InternalLink
+                    className="row ml-3 gap-1 hover:cursor-pointer hover:underline"
+                    to={editingFormPath(exam, contentType, data.id)}
+                    data-testid="edit-content-btn">
+                    <Icon name="muokkaa" color="text-green-primary" />
+                    <p className="text-green-primary">{t('assignment.muokkaa')}</p>
+                  </InternalLink>
                 </div>
               )}
+              {isAssignment(data, contentType) && (
+                <AssignmentContent
+                  assignment={data}
+                  exam={exam}
+                  teachingLanguage={teachingLanguageOverrideIfSukoAssignment}
+                  isPresentation={isPresentation}
+                />
+              )}
+
+              {isCertificate(data, contentType) && <CertificateContent certificate={data} />}
+
+              {isInstruction(data, contentType) && (
+                <InstructionContent
+                  instruction={data}
+                  teachingLanguage={teachingLanguage}
+                  isPresentation={isPresentation}
+                />
+              )}
             </div>
-            {!isPresentation && <div className="hidden w-3/12 flex-col border-l border-gray-separator md:flex" />}
           </div>
-        )}
-      />
+          {!isPresentation && (
+            <div className="row mb-6">
+              <Button variant="buttonSecondary" onClick={handleNavigation} data-testid="return">
+                {lt.returnTextByContentType[contentType]}
+              </Button>
+            </div>
+          )}
+        </div>
+        {!isPresentation && <div className="hidden w-3/12 flex-col border-l border-gray-separator md:flex" />}
+      </div>
     </div>
   )
 }
--- web/src/components/content/ContentError.tsx
+++ web/src/components/content/ContentError.tsx
@@ -1,11 +1,19 @@
 import { ContentType } from '../../types'
 import { useLudosTranslation } from '../../hooks/useLudosTranslation'
 import { Icon } from '../Icon'
+import { PageNotFound } from '../LudosRoutes'
 
-type ListErrorProps = { contentType: ContentType }
+type ListErrorProps = {
+  contentType: ContentType
+  error: string
+}
 
-export const ContentError = ({ contentType }: ListErrorProps) => {
+export const ContentError = ({ contentType, error }: ListErrorProps) => {
   const { lt } = useLudosTranslation()
+
+  if (error === '404') {
+    return <PageNotFound />
+  }
 
   return (
     <div className="flex justify-center w-full gap-2 text-red-primary mt-10">
