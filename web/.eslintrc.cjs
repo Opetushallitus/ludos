7ea2b14470b46e71b537dbdf01dad292b472733b
Christian Vilen <c.vilen@outlook.com>
2024-01-04 14:38:22 +0200

    Lisää jsx a11y eslint paketti
    - Static AST checker for accessibility rules on JSX elements

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -1,11 +1,14 @@
 const eslintConfig = {
-  extends: ['plugin:prettier/recommended'],
-  plugins: ['prettier', 'react-hooks'],
+  plugins: ['prettier', 'react-hooks', 'jsx-a11y'],
+  extends: ['plugin:prettier/recommended', "plugin:jsx-a11y/recommended"],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true
-    }, ecmaVersion: 2018, sourceType: 'module', project: './tsconfig.json'
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json'
   },
   rules: {
     curly: 'error',
