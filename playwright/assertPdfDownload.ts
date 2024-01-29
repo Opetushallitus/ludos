f940d147f0918f58eae38d0384188adbb2dc6cda
Christian Vilen <c.vilen@outlook.com>
2024-01-29 14:56:30 +0200

    Luo temp uniikkeja dirrejä per funktio kutsu

--- playwright/assertPdfDownload.ts
+++ playwright/assertPdfDownload.ts
@@ -1,6 +1,7 @@
 import { expect, Locator, Page } from '@playwright/test'
 import pdf from 'pdf-parse'
 import fs from 'fs'
+import path from 'path'
 
 export async function assertPDFDownload(
   page: Page,
@@ -10,27 +11,27 @@ export async function assertPDFDownload(
 ) {
   await expect(buttonLocator).toBeVisible()
 
+  const tempDir = fs.mkdtempSync(path.join(__dirname, 'temp_folder_for_tests_'))
+
   const [download] = await Promise.all([page.waitForEvent('download'), buttonLocator.click()])
 
   const filename = download.suggestedFilename()
   expect(filename).toBe(`${expectedFileTitle}.pdf`)
 
-  const tempPath = `temp_folder_for_tests/${filename}`
+  const tempPath = path.join(tempDir, filename)
   await download.saveAs(tempPath)
 
   const fileSizeInBytes = fs.statSync(tempPath).size
-
   expect(fileSizeInBytes).toBeGreaterThan(100)
   expect(fileSizeInBytes).toBeLessThan(200000)
 
   const data = await pdf(fs.readFileSync(tempPath))
-
   expect(data.numpages).toBe(1)
   expect(data.info.Title).toBe(expectedFileTitle)
 
   const cleanedText = data.text.replaceAll('\n', ' ')
   expect(cleanedText).toContain(expectedText)
 
-  // poista tiedosto
   fs.unlinkSync(tempPath)
+  fs.rmdirSync(tempDir)
 }
