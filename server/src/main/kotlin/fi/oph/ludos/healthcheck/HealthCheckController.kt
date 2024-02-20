cf0b7643a3d3751d694c9f229f80a78552584cd6
Christian Vilen <c.vilen@outlook.com>
2024-02-20 13:02:01 +0200

    Tee db kutsu ja palauta response entity

--- server/src/main/kotlin/fi/oph/ludos/healthcheck/HealthCheckController.kt
+++ server/src/main/kotlin/fi/oph/ludos/healthcheck/HealthCheckController.kt
@@ -2,23 +2,27 @@ package fi.oph.ludos.healthcheck
 
 import fi.oph.ludos.Constants
 import fi.oph.ludos.auth.RequireAtLeastYllapitajaRole
+import org.springframework.http.ResponseEntity
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
-data class HealthCheckResult(
-    val isHealthy: Boolean
-)
-
 @RestController
 @RequestMapping("${Constants.API_PREFIX}/health-check")
 @RequireAtLeastYllapitajaRole
-class HealthCheckController {
+class HealthCheckController(private val jdbcTemplate: JdbcTemplate) {
     @GetMapping("")
     @PreAuthorize("permitAll()")
-    fun getAssignment(): HealthCheckResult {
-        return HealthCheckResult(isHealthy = true)
+    fun healthCheck(): ResponseEntity<Nothing> {
+        try {
+            jdbcTemplate.query("SELECT 1") { rs, _ -> rs.getInt(1) }.firstOrNull()
+        } catch (e: Exception) {
+            return ResponseEntity.internalServerError().build()
+        }
+
+        return ResponseEntity.ok().build()
     }
 }
