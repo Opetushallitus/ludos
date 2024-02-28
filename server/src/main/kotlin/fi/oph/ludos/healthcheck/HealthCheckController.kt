bbd38c095bc3ba8ac5384a8662b1ec97a46cac8a
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2024-02-28 12:52:53 +0200

    Alustava logback-spring.xml

--- server/src/main/kotlin/fi/oph/ludos/healthcheck/HealthCheckController.kt
+++ server/src/main/kotlin/fi/oph/ludos/healthcheck/HealthCheckController.kt
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("${Constants.API_PREFIX}/health-check")
 @RequireAtLeastYllapitajaRole
 class HealthCheckController(private val jdbcTemplate: JdbcTemplate) {
+
     @GetMapping("")
     @PreAuthorize("permitAll()")
     fun healthCheck(): ResponseEntity<Nothing> {
