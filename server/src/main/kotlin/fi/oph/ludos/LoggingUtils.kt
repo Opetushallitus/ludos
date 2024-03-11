0d965d6f7a02cdc6ca19504e645560042d9e4396
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2024-03-11 13:16:05 +0200

    Lisää access-lokeihin real_remote_host X-Forwarded-For-headerista

--- server/src/main/kotlin/fi/oph/ludos/LoggingUtils.kt
+++ server/src/main/kotlin/fi/oph/ludos/LoggingUtils.kt
@@ -1,8 +1,12 @@
 package fi.oph.ludos
 
+import ch.qos.logback.access.spi.IAccessEvent
+import com.fasterxml.jackson.core.JsonGenerator
 import fi.oph.ludos.auth.Kayttajatiedot
 import fi.oph.ludos.auth.UserInfoForLogging
 import jakarta.servlet.ServletRequest
+import net.logstash.logback.composite.AbstractFieldJsonProvider
+import net.logstash.logback.composite.JsonWritingUtils
 import org.slf4j.spi.LoggingEventBuilder
 
 fun LoggingEventBuilder.addLudosUserInfo(): LoggingEventBuilder {
@@ -13,4 +17,16 @@ fun LoggingEventBuilder.addLudosUserInfo(): LoggingEventBuilder {
 fun LoggingEventBuilder.addUserIp(request: ServletRequest?): LoggingEventBuilder {
     addKeyValue("ip", request?.remoteAddr ?: "unknown")
     return this
+}
+
+class RealRemoteHostProvider() : AbstractFieldJsonProvider<IAccessEvent>() {
+    init {
+        fieldName = "real_remote_host"
+    }
+
+    override fun writeTo(generator: JsonGenerator?, event: IAccessEvent?) {
+        val remoteHostFromXforwardedFor =
+            event?.request?.getHeader("X-Forwarded-For")?.replace("\\s".toRegex(), "")?.split(",")?.firstOrNull()
+        JsonWritingUtils.writeStringField(generator, fieldName, remoteHostFromXforwardedFor ?: event?.remoteHost)
+    }
 }
