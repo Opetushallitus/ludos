b2371c7f628fb918c09b94371cac7a938043f6d3
Christian Vilen <c.vilen@outlook.com>
2024-03-12 09:51:09 +0200

    Käytä tila paremmin hyödyksi footerissa

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -14,25 +14,23 @@ export const Footer = ({ isPresentation }: FooterProps) => {
 
   return (
     <div
-      className="flex flex-wrap items-center justify-between text-xs mt-5 py-4 px-10 border-t-2 border-gray-separator"
+      className="row flex-wrap items-center text-xs mt-5 py-4 px-10 border-t-2 border-gray-separator"
       data-testid="footer">
-      <a href={OPH_URL} target="_blank" rel="noopener noreferrer" title="Opetushallituksen nettisivut">
-        <img className="h-12" src={logo} alt="Opetushallituksen logo" />
-      </a>
+      <div className="w-full md:w-1/3">
+        <a href={OPH_URL} target="_blank" rel="noopener noreferrer" title="Opetushallituksen nettisivut">
+          <img className="h-12" src={logo} alt="Opetushallituksen logo" />
+        </a>
+      </div>
       {!isPresentation && (
-        <div className="row flex-wrap gap-10 mt-3 text-center md:text-left">
-          <div>
-            <ExternalLink url={feedbackUrl} data-testid="feedback-link">
-              {t('footer.palaute')}
-            </ExternalLink>
-            <ExternalLink url={t('footer.kayttoohjeet-url')} data-testid="kayttoohjeet-link">
-              {t('footer.kayttoohjeet')}
-            </ExternalLink>
-          </div>
-          <div>
-            <ExternalLink url={virkailijanOpintopolkuUrl()}>{t('common.virkailijan-opintopolku')}</ExternalLink>
-            <ExternalLink url={TIETOSUOJA_SELOSTE_URL}>{t('footer.tietosuoja')}</ExternalLink>
-          </div>
+        <div className="flex flex-wrap w-full md:w-2/3 justify-evenly gap-5 mt-3 text-center md:text-left">
+          <ExternalLink url={feedbackUrl} data-testid="feedback-link">
+            {t('footer.palaute')}
+          </ExternalLink>
+          <ExternalLink url={t('footer.kayttoohjeet-url')} data-testid="kayttoohjeet-link">
+            {t('footer.kayttoohjeet')}
+          </ExternalLink>
+          <ExternalLink url={virkailijanOpintopolkuUrl()}>{t('common.virkailijan-opintopolku')}</ExternalLink>
+          <ExternalLink url={TIETOSUOJA_SELOSTE_URL}>{t('footer.tietosuoja')}</ExternalLink>
         </div>
       )}
     </div>
