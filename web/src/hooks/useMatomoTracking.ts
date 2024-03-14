40a293516594a3214233a5752fb8e6b7c02e672f
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2024-03-14 10:40:45 +0200

    Eka versio

---
+++ web/src/hooks/useMatomoTracking.ts
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+export const useMatomoTracking = () => {
+  const location = useLocation()
+
+  useEffect(() => {
+    const window = globalThis as any
+    window._paq = window._paq || []
+
+    // Track page view
+    window._paq.push(['setCustomUrl', window.location.href])
+    window._paq.push(['setDocumentTitle', document.title])
+    window._paq.push(['trackPageView'])
+  }, [location])
+}
