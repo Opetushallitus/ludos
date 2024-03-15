14a44786def972c5cbd360ca779b33eebe8aec01
Christian Vilen <c.vilen@outlook.com>
2024-03-15 08:39:22 +0200

    Korjaa roolin tarkistus pw testissä

--- playwright/auth.setup.ts
+++ playwright/auth.setup.ts
@@ -1,13 +1,15 @@
 import { expect, test as setup } from '@playwright/test'
-import { Role, authFileByRole, login } from './helpers'
+import { authFileByRole, login, Role } from './helpers'
 
 Object.values(Role).forEach((role: Role) => {
   setup(`authenticate as ${role}`, async ({ page }) => {
     await login(page, role)
+
     if (role === Role.UNAUTHORIZED) {
       await expect(page.getByTestId('unauthorizedPage')).toBeVisible()
     } else {
-      await expect(page.getByTestId('header-user-role')).toHaveText(role)
+      const expectedRoleText = role === Role.YLLAPITAJA ? 'ylläpitäjä' : 'opettaja'
+      await expect(page.getByTestId('header-user-role')).toHaveText(expectedRoleText)
     }
     await page.context().storageState({ path: authFileByRole[role] })
   })
