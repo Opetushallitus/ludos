c5f0378d4e88a1360711991a8f88d7c90599bc93
Visa de Bruijn <visa.de.bruijn@reaktor.com>
2024-03-19 15:04:39 +0200

    Consent-dialogi toimii

--- web/src/components/modal/ConsentModal.tsx
+++ web/src/components/modal/ConsentModal.tsx
@@ -1,28 +1,111 @@
-import React, { ReactNode, useState } from 'react'
+import React, { forwardRef, ForwardRefRenderFunction, useEffect, useImperativeHandle, useState } from 'react'
 import styles from './modal.module.css'
 import { Button } from '../Button'
-import { useTranslation } from 'react-i18next'
 import { useModal } from './useModal'
 import { ModalHeader } from './ModalHeader'
 import { twMerge } from 'tailwind-merge'
 import { ExternalLink } from '../ExternalLink'
 import { TIETOSUOJA_SELOSTE_URL } from '../../constants'
+import { getCookie, removeCookie, setCookie } from 'typescript-cookie'
+import { useLudosTranslation } from '../../hooks/useLudosTranslation'
 
-interface ConsentModalProps {}
+const CONSENT_MODEL_SHOWN_COOKIE_NAME = 'ludosConsentModalShown'
+const MATOMO_CONSENT_COOKIE_NAME = 'mtm_consent'
 
-export const ConsentModal = ({}: ConsentModalProps) => {
-  const { t } = useTranslation()
-  const [isOpen, setIsOpen] = useState(true)
-  const [isSettingsOpen, setIsSettingsOpen] = useState(true)
+function isConsentModalAlreadyShown(): boolean {
+  return !!getCookie(CONSENT_MODEL_SHOWN_COOKIE_NAME)
+}
+
+function rememberConsentModalShown() {
+  removeCookie(CONSENT_MODEL_SHOWN_COOKIE_NAME)
+  setCookie(CONSENT_MODEL_SHOWN_COOKIE_NAME, Date.now(), { expires: 365, path: '/' })
+}
+
+function isMatomoConsentGivenRemembered() {
+  return !!getCookie(MATOMO_CONSENT_COOKIE_NAME)
+}
+
+function rememberMatomoConsentGiven() {
+  const window = globalThis as any
+  window._paq = window._paq || []
+  window._paq.push(['rememberConsentGiven'])
+  window._paq.push(['rememberConsentGiven']) // rememberConsentGiven needs to be called twice to remove mtm_consent_removed
+}
+
+function forgetMatomoConsentGiven() {
+  const window = globalThis as any
+  window._paq = window._paq || []
+  window._paq.push(['forgetConsentGiven'])
+}
+
+function isTrackingConsentRemembered(): boolean {
+  return isMatomoConsentGivenRemembered()
+}
+
+function rememberTrackingConsentGiven() {
+  rememberMatomoConsentGiven()
+}
+
+function forgetTrackingConsentGiven() {
+  forgetMatomoConsentGiven()
+}
+
+export interface ConsentModalHandles {
+  setIsOpen: (isOpen: boolean) => void
+}
+
+const ConsentModalComponent: ForwardRefRenderFunction<ConsentModalHandles> = (_, ref) => {
+  const { t } = useLudosTranslation()
+  const [isOpen, setIsOpen] = useState(!isConsentModalAlreadyShown())
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false)
+  const [isTrackingConsentChecked, setIsTrackingConsentChecked] = useState(isTrackingConsentRemembered())
+  const autofocusRef = React.useRef<HTMLButtonElement>(null)
+
+  const resetModal = () => {
+    setIsSettingsOpen(false)
+    setIsTrackingConsentChecked(isTrackingConsentRemembered())
+  }
+
+  const resetAndSetIsOpen = (isOpen: boolean) => {
+    resetModal()
+    setIsOpen(isOpen)
+  }
+
+  useImperativeHandle(ref, () => ({
+    setIsOpen: resetAndSetIsOpen
+  }))
+
+  useEffect(() => {
+    if (isOpen) {
+      autofocusRef.current?.focus()
+    }
+  }, [isOpen])
+
   const onClose = () => setIsOpen(false)
-  const { modalRef, dialogClasses, onCancel, onAnimEnd } = useModal({ open: isOpen, onClose })
+  const { modalRef, dialogClasses, onAnimEnd } = useModal({ open: isOpen, onClose })
   const margin = 'm-6'
+
+  const onAcceptAll = () => {
+    rememberTrackingConsentGiven()
+
+    rememberConsentModalShown()
+    resetAndSetIsOpen(false)
+  }
+  const onAcceptSelected = () => {
+    if (isTrackingConsentChecked) {
+      rememberTrackingConsentGiven()
+    } else {
+      forgetTrackingConsentGiven()
+    }
+
+    rememberConsentModalShown()
+    resetAndSetIsOpen(false)
+  }
 
   return (
     <dialog
       ref={modalRef}
       className={dialogClasses}
-      /*onClose={onClose}*/
       onCancel={(e) => {
         e.preventDefault()
       }}
@@ -30,11 +113,11 @@ export const ConsentModal = ({}: ConsentModalProps) => {
       aria-modal="true"
       data-testid="consent-modal">
       <div className={styles['modal__container']}>
-        <ModalHeader modalTitle={t('consent.modal.title')} />
+        <ModalHeader modalTitle={t('consent.title')} />
 
         <div className="flex-grow" />
 
-        <p className={margin}>{t('consent.modal.general-info')}</p>
+        <p className={margin}>{t('consent.modal.info')}</p>
 
         <div className={margin}>
           <ExternalLink url={TIETOSUOJA_SELOSTE_URL}>{t('footer.tietosuoja')}</ExternalLink>
@@ -48,8 +131,14 @@ export const ConsentModal = ({}: ConsentModalProps) => {
               {t('consent.modal.checkbox.mandatory')}
             </label>
             <br />
-            <input type="checkbox" id="consent-mandatory-tracking" name="tracking" />
-            <label className="pl-2" htmlFor="consent-mandatory-checkbox">
+            <input
+              type="checkbox"
+              id="consent-tracking-checkbox"
+              checked={isTrackingConsentChecked}
+              readOnly
+              onClick={() => setIsTrackingConsentChecked(!isTrackingConsentChecked)}
+            />
+            <label className="pl-2" htmlFor="consent-tracking-checkbox">
               {t('consent.modal.checkbox.tracking')}
             </label>
           </div>
@@ -63,11 +152,11 @@ export const ConsentModal = ({}: ConsentModalProps) => {
             {isSettingsOpen ? t('consent.modal.hide-settings') : t('consent.modal.show-settings')}
           </Button>
           {isSettingsOpen && (
-            <Button variant="buttonSecondary" onClick={onClose} data-testid="accept-all-button">
+            <Button variant="buttonSecondary" onClick={onAcceptSelected} data-testid="accept-all-button">
               {t('consent.modal.accept-selected')}
             </Button>
           )}
-          <Button variant="buttonPrimary" onClick={onClose} data-testid="accept-all-button">
+          <Button variant="buttonPrimary" onClick={onAcceptAll} data-testid="accept-all-button" ref={autofocusRef}>
             {t('consent.modal.accept-all')}
           </Button>
         </div>
@@ -75,3 +164,5 @@ export const ConsentModal = ({}: ConsentModalProps) => {
     </dialog>
   )
 }
+
+export const ConsentModal = forwardRef(ConsentModalComponent)
