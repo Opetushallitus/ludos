a91f8b2cdf5e6484d11bb1b40a897caf9e1a688e
Riku Honkanen <riku.honkanen@reaktor.com>
2024-06-12 14:11:34 +0300

    OPHLUDOS-203-1: Run server tests in docker

--- deploy-scripts/02-run-server-tests.sh
+++ deploy-scripts/02-run-server-tests.sh
@@ -14,18 +14,36 @@ function main {
   require_docker_compose
   configure_aws_credentials
 
-  start_gh_actions_group "Build required web bundle"
-  "$repo"/scripts/build-web.sh
-  end_gh_actions_group
-
   start_gh_actions_group "Start DB for tests"
   docker compose up ludos-db --detach
   end_gh_actions_group
 
   start_gh_actions_group "Running gradle server tests"
+  docker build --target server-build --tag server-stage .
   pushd server
-  ./gradlew test --rerun-tasks
+  if running_on_gh_actions; then
+    docker run --env SPRING_PROFILES_ACTIVE=local \
+    --env DB_URL=jdbc:postgresql://host.docker.internal:5432/ludos \
+    --env AWS_ACCESS_KEY_ID \
+    --env AWS_SECRET_ACCESS_KEY \
+    --env AWS_SESSION_TOKEN \
+    --env AWS_REGION \
+    --env AWS_DEFAULT_REGION \
+    server-stage \
+    gradle test
+  else
+    docker run --env SPRING_PROFILES_ACTIVE=local \
+    --env DB_URL=jdbc:postgresql://host.docker.internal:5432/ludos \
+    --volume "${HOME}/.aws:/root/.aws" -v "$( pwd ):/aws" \
+    server-stage \
+    gradle test
+  fi
   popd
+  end_gh_actions_group
+
+
+  start_gh_actions_group "Stop DB for tests"
+  docker compose down ludos-db
   end_gh_actions_group
 }
 
