96d833c4387998b5011cfa6ff383dc4a65fcebe1
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2024-07-01 14:05:11 +0300

    OPHLUDOS-203-2: Lint Playwright tests
    This helps us debug flaky tests possibly caused by floating promises

--- playwright/.eslintrc.cjs
+++ playwright/.eslintrc.cjs
@@ -1,1 +1,14 @@
-../web/.eslintrc.cjs
+const eslintConfig = {
+  extends: ['plugin:@typescript-eslint/recommended-type-checked'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+}
+
+module.exports = eslintConfig
