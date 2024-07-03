ce8bed76314df34a0bcdc70199d0fd1334824ab0
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2024-07-03 16:48:02 +0300

    OPHLUDOS-203-2: Run npm ci before linting

--- deploy-scripts/04-lint.sh
+++ deploy-scripts/04-lint.sh
@@ -9,14 +9,14 @@ source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/deploy-functions.sh"
 
 function main {
     cd "$repo"
+    use_correct_node_version
 
     start_gh_actions_group "Lint web"
-    use_correct_node_version
     pushd web
+    npm_ci_if_package_lock_has_changed
     npm run lint
     popd
     end_gh_actions_group
-
 
     start_gh_actions_group "Lint playwright"
     pushd playwright
