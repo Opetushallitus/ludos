220780ea90db7eeb6c360d0515c67b9b73009f75
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2024-08-19 17:04:20 +0300

    OPHLUDOS-205-1: add worlkflow to automatically deploy to dev

---
+++ deploy-scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+# shellcheck source=../scripts/common-functions.sh
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../scripts/common-functions.sh"
+
+function main {
+  start_gh_actions_group "Setup"
+  parse_env_from_script_name "..-deploy"
+  use_correct_node_version
+  end_gh_actions_group
+
+  start_gh_actions_group "Deploy $ENV"
+  deploy
+  end_gh_actions_group
+}
+
+function deploy {
+  pushd "$repo"/infra
+
+  IMAGE_TAG="$ecr_image_tag" . "./cdk.sh" deploy --all
+
+  popd
+}
+
+main
