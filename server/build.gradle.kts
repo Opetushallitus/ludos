3cc6df49b793841740ad4de1e853135f25fd41a2
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2024-09-23 15:42:57 +0300

    Revert "Update plugin org.springframework.boot to v3.3.4"
    This reverts commit 69619ed9088c54f392a47017c0ef7991c983cec2.
    This fixes: Receiver class ch.qos.logback.access.tomcat.LogbackValve does not define or inherit an implementation of the resolved method 'abstract java.util.concurrent.locks.ReentrantLock getConfigurationLock()' of interface ch.qos.logback.core.Context.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,7 +3,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
-    id("org.springframework.boot") version "3.3.4"
+    id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "2.0.20"
     kotlin("plugin.spring") version "2.0.20"
