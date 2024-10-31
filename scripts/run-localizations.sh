15d03d027a42fb14dc377acba73718153d760fe0
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2024-10-31 16:45:37 +0200

    OPHLUDOS-228: Use correct node version when creating localizations

--- scripts/run-localizations.sh
+++ scripts/run-localizations.sh
@@ -5,6 +5,7 @@ source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/common-functions.sh"
 
 function main () {
   pushd "$repo"/localizations
+  use_correct_node_version
   npm_ci_if_package_lock_has_changed
   node --no-warnings --loader @swc-node/register/esm localizations.ts "$@"
   popd
