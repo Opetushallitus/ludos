e542b3f57f3ac815052ea0659c6fe84c3b58fac6
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2024-10-31 19:13:15 +0200

    OPHLUDOS-228: Permit only /api/config/features to be accessed without authentication
    This fixes test:
    AuthorizationTest > all controllers should have @RequireAtLeastYllapitajaRole annotation() FAILED

--- server/src/main/kotlin/fi/oph/ludos/config/ConfigController.kt
+++ server/src/main/kotlin/fi/oph/ludos/config/ConfigController.kt
@@ -1,9 +1,11 @@
 package fi.oph.ludos.config
 
 import fi.oph.ludos.Constants
+import fi.oph.ludos.auth.RequireAtLeastYllapitajaRole
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("${Constants.API_PREFIX}/config")
+@RequireAtLeastYllapitajaRole
 class ConfigController(val features: Features) {
     @GetMapping("/features", produces = ["application/json"])
     @ResponseBody
+    @PreAuthorize("permitAll()")
     fun getFeatures(): ResponseEntity<FeaturesResponse> {
         return ResponseEntity.ok(FeaturesResponse(features.tehtavaPalauteLinkki))
     }
