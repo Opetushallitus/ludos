758e9a0cc5f4b2e9c3c9f8ce669c1db43b9635ef
Riku Honkanen <riku.honkanen@reaktor.com>
2024-11-20 12:48:01 +0200

    OPHLUDOS-232-3-2: Wait for services to be completely stopped so there is no more database connections left

--- copyEnvState/index.ts
+++ copyEnvState/index.ts
@@ -1,40 +1,49 @@
 #!/usr/bin/env zx
 import 'zx/globals'
-import { ECSClient, UpdateServiceCommand, UpdateServiceRequest, waitUntilServicesStable } from '@aws-sdk/client-ecs'
+import {
+  ECSClient,
+  UpdateServiceCommand,
+  UpdateServiceRequest,
+  waitUntilServicesStable,
+  ListTasksCommand,
+  DescribeTasksCommand
+} from '@aws-sdk/client-ecs'
+import { setTimeout } from 'timers/promises'
 
 const client = new ECSClient({ region: 'eu-west-1' })
+const cluster = 'UntuvaCluster'
+const service = 'UntuvaService'
 
 const takeDownServicesRequest: UpdateServiceRequest = {
-  cluster: 'UntuvaCluster',
-  service: 'UntuvaService',
-  desiredCount: 0
+  desiredCount: 0,
+  cluster,
+  service
 }
 const upServicesRequest: UpdateServiceRequest = {
-  cluster: 'UntuvaCluster',
-  service: 'UntuvaService',
+  cluster,
+  service,
   desiredCount: 2
 }
 
 async function main() {
+  await takeServiceDown()
+  await bringServiceUp()
+}
+
+async function takeServiceDown() {
   console.log('Take down services')
   const takeDownServicesCommand = new UpdateServiceCommand(takeDownServicesRequest)
   const downServiceResponse = await client.send(takeDownServicesCommand)
   console.log(`desiredCount: ${downServiceResponse.service?.desiredCount}`)
   console.log('🧨 Polling for services to be taken down...')
-
-  const downPollResult = await waitUntilServicesStable(
-    { client, maxWaitTime: 500 },
-    {
-      cluster: 'UntuvaCluster',
-      services: ['UntuvaService']
-    }
-  )
-
-  if (downPollResult.state !== 'SUCCESS') {
-    throw new Error(`failed to take services down. Poll status ${downPollResult}`)
+  const allStopped = await pollForStoppedTasks()
+  if (!allStopped) {
+    throw new Error('Tasks not stopped after 100 seconds')
   }
   console.log('Successfully taken down services')
+}
 
+async function bringServiceUp() {
   console.log('\nBring up services')
   const bringUpServicesCommand = new UpdateServiceCommand(upServicesRequest)
   const upServiceResponse = await client.send(bringUpServicesCommand)
@@ -53,6 +62,64 @@ async function main() {
     throw new Error(`failed to bring services up. Poll status ${upPollResults}`)
   }
   console.log('Successfully brought services up')
+}
+
+async function pollForStoppedTasks() {
+  await poll(noDesiredStatusRunningTasks)
+
+  const listStoppedCommand = new ListTasksCommand({
+    cluster,
+    serviceName: service,
+    desiredStatus: 'STOPPED'
+  })
+
+  const { taskArns } = await client.send(listStoppedCommand)
+  if (!taskArns || taskArns.length === 0) {
+    throw new Error('No tasks with desired status "STOPPED"')
+  }
+  const allStopped = await poll(() => allTasksStopped(taskArns))
+  console.log({ allStopped })
+  return allStopped
+}
+
+async function noDesiredStatusRunningTasks() {
+  const listRunningCommand = new ListTasksCommand({
+    cluster,
+    serviceName: service,
+    desiredStatus: 'RUNNING'
+  })
+
+  const { taskArns } = await client.send(listRunningCommand)
+  return taskArns?.length === 0
+}
+
+async function allTasksStopped(taskArns: string[]) {
+  const describeCommand = new DescribeTasksCommand({
+    cluster,
+    tasks: taskArns
+  })
+
+  console.log(`Fetching describe for tasks: ${taskArns.join(', ')}`)
+  const { tasks } = await client.send(describeCommand)
+  console.log(`Fetch success, Checking that all task are stopped`)
+  // console.log(statuses)
+  if (!tasks) {
+    throw new Error('Didnt find tasks for task Arns')
+  }
+  return tasks.every((task) => task.lastStatus === 'STOPPED')
+}
+
+async function poll(func: () => Promise<boolean>, retriesLeft = 40): Promise<boolean> {
+  if (retriesLeft === 0) {
+    throw new Error('Failed to get succesfull status in 20 tries')
+  }
+  const success = await func()
+  if (!success) {
+    console.log(`polling again in 15 second, retries left: ${retriesLeft}`)
+    await setTimeout(15000)
+    return await poll(func, retriesLeft - 1)
+  }
+  return success
 }
 
 void main()
