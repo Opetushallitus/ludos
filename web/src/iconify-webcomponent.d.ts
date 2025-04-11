f8599f50248edfefed054b3325b9edf510c91ac7
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2025-04-11 15:55:28 +0300

    LUDOS-213: Add rest of the types of iconify-icon

--- web/src/iconify-webcomponent.d.ts
+++ web/src/iconify-webcomponent.d.ts
@@ -1,5 +1,7 @@
 declare namespace JSX {
+  import type { IconifyIconProperties } from 'iconify-icon'
+
   interface IntrinsicElements {
-    'iconify-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { icon: string }, HTMLElement>
+    'iconify-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & IconifyIconProperties, HTMLElement>
   }
 }
