f97e55ccbc09e8c215e73955a36cf0ee49ca1b59
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2025-04-11 16:13:50 +0300

    LUDOS-213: Make CSP more strict

--- server/src/main/kotlin/fi/oph/ludos/auth/CspManager.kt
+++ server/src/main/kotlin/fi/oph/ludos/auth/CspManager.kt
@@ -1,11 +1,11 @@
 package fi.oph.ludos.auth
 
 object CspManager {
-    val defaultSrc: String = "'self'"
-    val scriptSrc: String = "'self'"
-    val imgSrc: String = "'self'"
-    val styleSrc: String = "* 'unsafe-inline'"
-    val scriptSrcElem: String = "'self' 'unsafe-inline' analytiikka.opintopolku.fi"
+    private const val defaultSrc: String = "'self'"
+    private const val scriptSrc: String = "'self'"
+    private const val imgSrc: String = "'self'"
+    private const val styleSrc: String = "'self' 'unsafe-inline'"
+    private const val scriptSrcElem: String = "'self' 'unsafe-inline' analytiikka.opintopolku.fi"
 
     fun makeCSPString(): String = """
     default-src $defaultSrc;
