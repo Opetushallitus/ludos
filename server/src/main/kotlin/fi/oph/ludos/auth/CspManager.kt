a45d0da5f78ef5752c12edae32c1f48722cb9c5d
Vilppu Saarinen <vilppu.saarinen@reaktor.com>
2025-04-11 15:04:06 +0300

    LUDOS-213-CSP Set all CSPs but styleSrc

--- server/src/main/kotlin/fi/oph/ludos/auth/CspManager.kt
+++ server/src/main/kotlin/fi/oph/ludos/auth/CspManager.kt
@@ -3,8 +3,15 @@ package fi.oph.ludos.auth
 object CspManager {
     val defaultSrc: String = "'self'"
     val scriptSrc: String = "'self'"
-    val imgSrc: String = ""
-    val styleSrc: String = ""
+    val imgSrc: String = "'self'"
+    val styleSrc: String = "* 'unsafe-inline'"
+    val scriptSrcElem: String = "'self' 'unsafe-inline' analytiikka.opintopolku.fi"
 
-    fun makeCSPString(): String = "default-src $defaultSrc; sript-src $scriptSrc; img-src $imgSrc; style-src $styleSrc"
+    fun makeCSPString(): String = """
+    default-src $defaultSrc;
+    sript-src $scriptSrc;
+    img-src $imgSrc;
+    style-src $styleSrc;
+    script-src-elem $scriptSrcElem;
+        """.replace("\n", " ").trim()
 }
