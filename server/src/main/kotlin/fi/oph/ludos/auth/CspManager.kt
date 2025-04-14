bd9a422f7e8e325bf6b6f077ccc906d6eae5003a
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2025-04-14 12:35:00 +0300

    LUDOS-213: Don't allow inline styles

--- server/src/main/kotlin/fi/oph/ludos/auth/CspManager.kt
+++ server/src/main/kotlin/fi/oph/ludos/auth/CspManager.kt
@@ -4,7 +4,7 @@ object CspManager {
     private const val defaultSrc: String = "'self'"
     private const val scriptSrc: String = "'self'"
     private const val imgSrc: String = "'self'"
-    private const val styleSrc: String = "'self' 'unsafe-inline'"
+    private const val styleSrc: String = "'self'"
     private const val scriptSrcElem: String = "'self' 'unsafe-inline' analytiikka.opintopolku.fi"
 
     fun makeCSPString(): String = """
