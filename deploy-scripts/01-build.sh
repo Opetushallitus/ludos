47954d11655bdef07fc5cb1cbab48d5e30e6bdec
Riku Honkanen <riku.honkanen@reaktor.com>
2025-05-19 09:21:59 +0300

    Try other path

--- deploy-scripts/01-build.sh
+++ deploy-scripts/01-build.sh
@@ -44,7 +44,7 @@ function main {
     for tag in "${tags_to_push[@]}"
     do
       info "docker push $tag"
-      docker push "$tag"
+      # docker push "$tag"
     done
     end_gh_actions_group
   else
