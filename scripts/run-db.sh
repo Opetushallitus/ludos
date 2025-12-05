a8d736540d0debc932d599a9a86f241594209489
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2025-12-05 16:50:45 +0200

    LUDOS-264: Remove unnecessary docker-compose files

--- scripts/run-db.sh
+++ scripts/run-db.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-#
 set -o errexit -o nounset -o pipefail
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/common-functions.sh"
 
-function main () {
-  docker compose -f "$repo"/docker-compose-db.yaml up
+function main {
+  pushd $repo
+  docker compose up ludos-db
+  popd
 }
 
-main
+main "$@"
--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
-#
 set -o errexit -o nounset -o pipefail
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/common-functions.sh"
 
 function main () {
-  pushd "$repo"/server
 
-  docker compose -f "$repo"/docker-compose-run-server.yaml up --watch
+  require_aws_session_for_env dev
+  initialize_credentials
 
+  pushd "$repo"
+  DB_URL=jdbc:postgresql://ludos-db:5432/ludos \
+  SPRING_PROFILES_ACTIVE=local \
+    docker compose up --build ludos
   popd
+}
+
+function initialize_credentials() {
+  LUDOS_PALVELUKAYTTAJA_JSON=$(aws --profile oph-ludos-dev secretsmanager get-secret-value --secret-id /UntuvaLudosStack/LudosApplicationStack/OphServiceUserCredentials --query 'SecretString' | jq -r .)
+  export "LUDOS_PALVELUKAYTTAJA_USERNAME=\"$(echo "$LUDOS_PALVELUKAYTTAJA_JSON" | jq -r .username)\""
+  export "LUDOS_PALVELUKAYTTAJA_PASSWORD=\"$(echo "$LUDOS_PALVELUKAYTTAJA_JSON" | jq -r .password)\""
 }
 
 main
