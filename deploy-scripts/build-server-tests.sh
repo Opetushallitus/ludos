864fbfcf516efe2e3e3e21963508bac0d95beccc
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2026-02-27 16:57:20 +0200

    ci: run lint and build in parallel for shard branch

---
+++ deploy-scripts/build-server-tests.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+# shellcheck source=../scripts/common-functions.sh
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../scripts/common-functions.sh"
+
+# shellcheck source=./deploy-functions.sh
+source "$repo/deploy-scripts/deploy-functions.sh"
+
+# shellcheck source=./02-run-server-tests.sh
+source "$repo/deploy-scripts/02-run-server-tests.sh"
+
+function main {
+  build
+  test-server
+}
+
+function build {
+  pushd "$repo"
+  start_gh_actions_group "Building ludos-server"
+
+  require_command docker
+  require_docker_compose
+
+  LUDOS_TAG="ludos-$revision"
+  export LUDOS_TAG
+  docker compose -f ./docker-compose.yaml build ludos-server
+  end_gh_actions_group
+  popd
+}
+
+main
