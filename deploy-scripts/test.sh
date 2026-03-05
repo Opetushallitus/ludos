d96b0afb6d29026aea9c8b8eec725b206f15ae36
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2026-03-05 13:05:12 +0200

    LUDOS-276 Split build, lint and test into separate parallel jobs

---
+++ deploy-scripts/test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+# shellcheck source=../scripts/common-functions.sh
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../scripts/common-functions.sh"
+
+# shellcheck source=./deploy-functions.sh
+source "$repo/deploy-scripts/deploy-functions.sh"
+
+# shellcheck source=./02-run-server-tests.sh
+source "$repo/deploy-scripts/02-run-server-tests.sh"
+
+# shellcheck source=./03-run-playwright-tests.sh
+source "$repo/deploy-scripts/03-run-playwright-tests.sh"
+
+function main {
+  test-server
+  playwright-test
+}
+
+main
